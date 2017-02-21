1c3742e12ea7075d2d531960baf320682f7aba8f
perfectacle <perfectacle@naver.com>
2017-02-21 18:18:01 +0900

    세미콜론 추가

--- app/src/app.js
+++ app/src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router, Route, browserHistory } from 'react-router'
+import { Router, Route, browserHistory } from 'react-router';
 
 import store from './store';
 import Container from './containers/Container';
